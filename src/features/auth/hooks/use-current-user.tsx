a77e22190678eb13e187c3e53ef73fbedafd49c8
matthewloh <64674205+matthewloh@users.noreply.github.com>
2024-09-08 03:34:00 +0800

    attempt to fix use current user hook

--- src/features/auth/hooks/use-current-user.tsx
+++ src/features/auth/hooks/use-current-user.tsx
@@ -1,26 +1,16 @@
 "use client"
 import { createClient } from "@/utils/supabase/client"
 import { useQuery } from "@tanstack/react-query"
-import { User } from "@supabase/supabase-js"
-
-const initialUser = {
-    created_at: "",
-    display_name: "",
-    email: "",
-    id: "",
-    image_url: "",
-}
 
 export function useUser() {
     return useQuery({
         queryKey: ["current-user"],
         queryFn: async () => {
             const supabase = createClient()
-            const { data } = await supabase.auth.getUser()
-            if (data.user) {
-                return data.user
-            }
-            return initialUser
+            const {
+                data: { user },
+            } = await supabase.auth.getUser()
+            return user
         },
     })
 }
