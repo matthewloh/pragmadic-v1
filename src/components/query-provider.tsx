a32296d30d0eedd98f3edb3bf1dd90843cf5e5b1
matthewloh <64674205+matthewloh@users.noreply.github.com>
2024-09-08 03:19:46 +0800

    rework query client

--- src/components/query-provider.tsx
+++ src/components/query-provider.tsx
@@ -14,7 +14,7 @@ function makeQueryClient() {
             queries: {
                 // With SSR, we usually want to set some default staleTime
                 // above 0 to avoid refetching immediately on the client
-                staleTime: Infinity,
+                staleTime: 1000 * 60 * 5, // 5 minutes 
             },
         },
     })
